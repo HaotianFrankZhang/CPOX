34c0dfcd9e21ae57b4ba3a7d347d72bc3a93a8a2
Haotian Zhang <118850100+HaotianFrankZhang@users.noreply.github.com>
2025-05-05 14:29:36 -0400

    Add files via upload

---
+++ Docking_AF/G188Q/RunAll.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Loop through each mutation folder
+for folder in G188Q_*
+do
+    # Loop through each run folder within the mutation folder
+    for run in "$folder"/Run*
+    do
+        # Loop through each target folder (copro, uro) within the run folder
+        for target in copro uro
+        do
+            # Change directory to the target folder
+            cd "$run/$target"
+            # Execute the sbatch command
+            sbatch Vina_job
+            # Return to the original directory
+            cd - >/dev/null
+        done
+    done
+done
